b15c45599a7665c02fedd869c7ab9caa35c3054e
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-11-01 19:16:02 -0300

    Finalizando implementação de filtros

--- src/app/modules/usuario/pages/usuarioLista/usuarioLista.page.ts
+++ src/app/modules/usuario/pages/usuarioLista/usuarioLista.page.ts
@@ -12,10 +12,20 @@ import { Utils } from "src/app/core/utils";
 export class UsuarioListaPage implements OnInit { 
 
     //Área de atributos, objetos e variáveis
-    usuarioFiltro: any[] = [];
-    formFiltro: FormGroup;
-    filtroTodosUsuarios: any[] = [];
 
+    private _registros: any | undefined;
+
+    nomeFiltro: any[] = [];
+    filtroTodosNome: any[] = [];
+
+    emailFiltro: any [] = [];
+    filtroTodosEmail: any[] = [];
+
+    senhaFiltro: any [] = [];
+    filtroTodasSenha: any [] = [];
+
+    formFiltro: FormGroup;
+    resultadoFiltro: any[] = [];
     constructor( 
         protected router : Router,
         protected formBuilder : FormBuilder,
@@ -23,7 +33,13 @@ export class UsuarioListaPage implements OnInit {
     ){
         this.formFiltro = this.CriarForm();
         this.utils.tratarAutoCompleteEntidade(this.formFiltro.controls['nomeUsuario'],
-            this.formFiltro.controls['idUsuario'], this.aplicarFiltroUsuario.bind(this), 0);
+            this.formFiltro.controls['idUsuario'], this.aplicarFiltroNome.bind(this), 0);
+
+            this.utils.tratarAutoCompleteEntidade(this.formFiltro.controls['emailUsuario'],
+                this.formFiltro.controls['idEmail'], this.aplicarFiltroEmail.bind(this), 0);
+
+                this.utils.tratarAutoCompleteEntidade(this.formFiltro.controls['senhaUsuario'],
+                    this.formFiltro.controls['idSenha'], this.aplicarFiltroSenha.bind(this), 0);
     }
 
     //Área de tratamento autocomplete
@@ -34,24 +50,84 @@ export class UsuarioListaPage implements OnInit {
     private CriarForm() : FormGroup{
         return this.formBuilder.group({
             nomeUsuario: [null, []],
-            idUsuario: [null, []]
+            idUsuario: [null, []],
+
+            emailUsuario: [null, []],
+            idEmail: [null, []],
+
+            senhaUsuario: [null, []],
+            idSenha: [null, []]
         },
         {updateOn : "change"}
     );
     }
 
-    AplicarFiltro(){
-        
+    get registro() : any[]
+    {
+        return this._registros;
     }
 
-    aplicarFiltroUsuario(pUsuario : any){
-        this.usuarioFiltro = this.filtroTodosUsuarios.filter(f=>{
+    AplicarFiltro(){
+        this.resultadoFiltro = this.registro;
+        const lNome = this.formFiltro.controls['nomeUsuario'].value;
+        const lEmail = this.formFiltro.controls['emailUsuario'].value;
+        const lSenha = this.formFiltro.controls['senhaUsuario'].value;
+
+        if(lNome !== '' && lNome !==undefined && lNome !== null){
+            const nomeString = `${lNome.nome}`;
+            this.resultadoFiltro = this.resultadoFiltro.filter(f=>{
+                const registroNomeString = `${f.nome}`;
+                return registroNomeString.includes(nomeString);
+            });
+        }
+
+        if(lEmail !== '' && lEmail !==undefined && lEmail !== null){
+            const emailString = `${lEmail.email}`;
+            this.resultadoFiltro = this.resultadoFiltro.filter(f=>{
+                const registroEmailString = `${f.email}`;
+                return registroEmailString.includes(emailString);
+            });
+        }
+
+        if(lSenha !== '' && lSenha !==undefined && lSenha !== null){
+            const senhaString = `${lSenha.senha}`;
+            this.resultadoFiltro = this.resultadoFiltro.filter(f=>{
+                const registroSenhaString = `${f.senha}`;
+                return registroSenhaString.includes(senhaString);
+            });
+        }
+    }
+
+    aplicarFiltroNome(pNome : any){
+        this.nomeFiltro = this.filtroTodosNome.filter(f=>{
             const usuarioString = `${f.nome}`;
-            return usuarioString.includes(pUsuario);
+            return usuarioString.includes(pNome);
         });
     }
 
-    obterUsuario(pEntidade:any): any{
+    aplicarFiltroEmail(pEmail : any){
+        this.emailFiltro = this.filtroTodosEmail.filter(f=>{
+            const usuarioString = `${f.email}`;
+            return usuarioString.includes(pEmail);
+        });
+    }
+
+    aplicarFiltroSenha(pSenha : any){
+        this.senhaFiltro = this.filtroTodasSenha.filter(f=>{
+            const usuarioString = `${f.senha}`;
+            return usuarioString.includes(pSenha);
+        });
+    }
+
+    obterNome(pEntidade:any): any{
         return pEntidade ? `${pEntidade.nome}` : undefined;
+    }
+
+    obterEmail(pEntidade:any) : any{
+        return pEntidade ? `${pEntidade.email}` : undefined;
+    }
+
+    obterSenha(pEntidade:any) : any{
+        return pEntidade ? `${pEntidade.senha}` : undefined;
     }
 }
